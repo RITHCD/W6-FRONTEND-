8900c5eb5fb4a6ee0f992a3cf5f9ffae984d7269
HEM ROTHAPANHARITH <GMRithzz123@gmail.com>
2026-02-26 15:50:11 +0700

    ex5

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import pnLogo from "./assets/pn-logo.png";
 import Header from "./components/Header";
 import Scores from "./components/Scores";
 import { JAVA_RESULTS } from "./data.js";
+import { PYTHON_RESULTS } from "./data.js";
+import { HTML_RESULTS } from "./data.js";
+import { ENGLISH_RESULTS } from "./data.js";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
 
       <main className="scores-container">
         <Scores courseName="JAVA" courseResults={JAVA_RESULTS} />
+        <Scores courseName="PYTHON" courseResults={PYTHON_RESULTS} />
+        <Scores courseName="HTML" courseResults={HTML_RESULTS} />
+        <Scores courseName="ENGLISH" courseResults={ENGLISH_RESULTS} />
       </main>
     </>
   );
